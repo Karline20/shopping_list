fbb37939c52024ec3093974a38589803436fd2ec
Karlen Legaspi <karlen.legaspi20@gmail.com>
2025-02-10 00:05:38 +0800

    Added handling error in grocery list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -17,6 +17,7 @@ class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
 
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -31,6 +32,14 @@ class _GroceryListState extends State<GroceryList> {
       url,
       headers: {'Content-type': 'application/json'},
     );
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later';
+      });
+    }
+
+    print(response.statusCode);
     final Map<String, dynamic> shoppingList = json.decode(response.body);
 
     final List<GroceryItem> loadedItems = [];
@@ -110,6 +119,12 @@ class _GroceryListState extends State<GroceryList> {
             ),
           ),
         ),
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
       );
     }
 
