8a9b7fb866f4048ab69e6e398ffe193442de6844
Karlen Legaspi <karlen.legaspi20@gmail.com>
2025-02-09 23:58:28 +0800

    Added isSending circular progress indicator

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -18,10 +18,14 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
       final url = Uri.https('dummy-backend-b3536-default-rtdb.firebaseio.com',
           'shopping-list.json');
       final response = await http.post(
@@ -159,18 +163,26 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: Text(
                       'Reset',
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,
-                    child: Text(
-                      'Add Item',
-                    ),
+                    onPressed: _isSending ? null : _saveItem,
+                    child: _isSending
+                        ? SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : Text(
+                            'Add Item',
+                          ),
                   ),
                 ],
               )
